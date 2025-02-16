887e52da576fe19fd4d9733565f6e8d5b873f9df
beshoycoding123 <beshoy.shawky.2013@gmail.com>
2025-02-16 12:45:14 +0400

    identity operator

---
+++ identityoperator.py
@@ -0,0 +1,21 @@
+a=66
+if (type(a) is int):
+    print("True")
+else:
+    print("False")
+b=55.4
+if (type(b) is not float):
+    print("False")
+else:
+    print("True")
+x=66
+y=55.4
+if (x is y):
+    print("x and y have the same identity")
+else:
+    print("x and y don't have the same identity")
+y=66
+if (x is y):
+    print("x and y have the same identity")
+else:
+    print("x and y don't have the same identity")
